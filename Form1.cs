a649f7e22468f24797abb3d6d7890302aef67c7d
Astralist <83104822+Astralist@users.noreply.github.com>
2021-04-24 15:11:34 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if (output.Text == "0") 
+            {
+                output.Clear();
+            }
+            Button button = (Button)sender;
+            output.Text = output.Text + button.Text;
+        }
+    }
+}
